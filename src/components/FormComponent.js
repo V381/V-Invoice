18903c1546e6969be3d77a67011726520e62a0ff
V381 <paavlee@yahoo.com>
2024-03-22 02:34:49 +0100

    Input Field component + refactoring

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import isEqual from 'lodash/isEqual';
+import InputField from './InputField';
 
 import {
   updateFormData,
@@ -112,208 +113,172 @@ function FormComponent( {cardData, isEditing, onCloseForm} ) {
       {formError && <p className={styles.error}>{formError}</p>}
 
       <div className={`${styles.formField} ${styles.clientName}`}>
-        <label className={styles.label} htmlFor="clientName">
-          Client's Name:
-        </label>
-        <input
-          className={styles.inputField}
-          type="text"
+        <InputField
+          label="Client's Name"
           id="clientName"
           name="clientName"
           defaultValue={cardData?.clientName}
           onChange={handleChange}
+          type="text" 
         />
       </div>
 
       <div className={`${styles.formField} ${styles.clientEmail}`}>
-        <label className={styles.label} htmlFor="clientEmail">
-          Client's Email:
-        </label>
-        <input
-          className={styles.inputField}
-          type="email"
+        <InputField
+          label="Client's Email"
           id="clientEmail"
           name="clientEmail"
           defaultValue={cardData?.clientEmail}
           onChange={handleChange}
+          type="email"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.streetAddress}`}>
-        <label className={styles.label} htmlFor="streetAddress">
-          Street Address:
-        </label>
-        <input
-          className={styles.inputField}
-          type="text"
+        <InputField
+          label="Street Address"
           id="streetAddress"
           name="streetAddress"
           defaultValue={cardData?.streetAddress}
           onChange={handleChange}
+          type="text"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.city}`}>
-        <label className={styles.label} htmlFor="city">
-          City:
-        </label>
-        <input
-          className={styles.inputField}
-          type="text"
+        <InputField
+          label="City"
           id="city"
           name="city"
           defaultValue={cardData?.city}
           onChange={handleChange}
+          type="text"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.zipCode}`}>
-        <label className={styles.label} htmlFor="zipCode">
-          Zip Code:
-        </label>
-        <input
-          className={styles.inputField}
-          type="text"
+        <InputField
+          label="Zip Code"
           id="zipCode"
           name="zipCode"
           defaultValue={cardData?.zipCode}
           onChange={handleChange}
+          type="text"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.country}`}>
-        <label className={styles.label} htmlFor="country">
-          Country:
-        </label>
-        <input
-          className={styles.inputField}
-          type="text"
+        <InputField
+          label="Country"
           id="country"
           name="country"
           defaultValue={cardData?.country}
           onChange={handleChange}
+          type="text"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.invoiceDate}`}>
-        <label className={styles.label} htmlFor="invoiceDate">
-          Invoice Date:
-        </label>
-        <input
-          className={styles.inputField}
-          type="date"
+        <InputField
+          label="Invoice Date"
           id="invoiceDate"
           name="invoiceDate"
           defaultValue={cardData?.invoiceDate}
           onChange={handleChange}
+          type="date"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.paymentDue}`}>
-        <label className={styles.label} htmlFor="paymentDue">
-          Payment Due:
-        </label>
-        <input
-          className={styles.inputField}
-          type="date"
+        <InputField
+          label="Payment Due"
           id="paymentDue"
           name="paymentDue"
           defaultValue={cardData?.paymentDue}
           onChange={handleChange}
+          type="date"
         />
       </div>
 
       <div className={`${styles.formField} ${styles.paymentTerms}`}>
-  <label className={styles.label} htmlFor="paymentTerms">
-    Payment Terms:
-  </label>
-  <select
-    className={`${styles.inputField} ${styles.selectField}`} 
-    id="paymentTerms"
-    name="paymentTerms"
-    defaultValue={cardData?.paymentTerms}
-    onChange={handleChange}
-  >
-    <option value="net-30">Net 30</option>
-    <option value="net-60">Net 60</option>
-    <option value="net-90">Net 90</option>
-  </select>
-</div>
+        <label className={styles.label} htmlFor="paymentTerms">
+          Payment Terms:
+        </label>
+        <select
+          className={`${styles.inputField} ${styles.selectField}`} 
+          id="paymentTerms"
+          name="paymentTerms"
+          defaultValue={cardData?.paymentTerms}
+          onChange={handleChange}
+        >
+          <option value="net-30">Net 30</option>
+          <option value="net-60">Net 60</option>
+          <option value="net-90">Net 90</option>
+        </select>
+      </div>
 
-<div className={`${styles.formField} ${styles.productDescription}`}>
-  <label className={styles.label} htmlFor="productDescription">
-    Product Descriptions:
-  </label>
-  <select
-    className={`${styles.inputField} ${styles.selectField}`} 
-    id="productDescription"
-    name="productDescription"
-    defaultValue={cardData?.productDescription}
-    onChange={handleChange}
-  >
-    <option value="product-1">Front End</option>
-    <option value="product-2">Back End</option>
-    <option value="product-3">UI/UX</option>
-    <option value="product-4">Project Management</option>
-  </select>
-</div>
+      <div className={`${styles.formField} ${styles.productDescription}`}>
+        <label className={styles.label} htmlFor="productDescription">
+          Product Descriptions:
+        </label>
+        <select
+          className={`${styles.inputField} ${styles.selectField}`} 
+          id="productDescription"
+          name="productDescription"
+          defaultValue={cardData?.productDescription}
+          onChange={handleChange}
+        >
+          <option value="product-1">Front End</option>
+          <option value="product-2">Back End</option>
+          <option value="product-3">UI/UX</option>
+          <option value="product-4">Project Management</option>
+        </select>
+      </div>
 
 
       <div className={styles.row}>
         <div className={`${styles.formField} ${styles.itemName}`}>
-          <label className={styles.label} htmlFor="itemName">
-            Item Name:
-          </label>
-          <input
-            className={`${styles.inputField} ${styles.itemName}`}
-            type="text"
+          <InputField
+            label="Item Name"
             id="itemName"
             name="itemName"
             defaultValue={cardData?.itemName}
             onChange={handleChange}
+            type="text"
           />
         </div>
 
         <div className={`${styles.formField} ${styles.city}`}>
-          <label className={styles.label} htmlFor="itemCity">
-            City:
-          </label>
-          <input
-            className={`${styles.inputField} ${styles.city}`}
-            type="text"
-            id="itemCity"
-            name="itemCity"
-            defaultValue={cardData?.itemCity} 
-            onChange={handleChange}
-          />
+        <InputField
+          label="Item City"
+          id="itemCity"
+          name="itemCity"
+          defaultValue={cardData?.itemCity}
+          onChange={handleChange}
+          type="text"
+        />
         </div>
 
         <div className={`${styles.formField} ${styles.price}`}>
-          <label className={styles.label} htmlFor="price">
-            Price:
-          </label>
-          <input
-            className={`${styles.inputField} ${styles.price}`}
-            type="text"
-            id="itemPrice"
-            name="itemPrice"
-            defaultValue={cardData?.itemPrice}
-            onChange={handleChange}
-          />
+        <InputField
+          label="Price"
+          id="itemPrice"
+          name="itemPrice"
+          defaultValue={cardData?.itemPrice}
+          onChange={handleChange}
+          type="text"
+        />
         </div>
 
         <div className={`${styles.formField} ${styles.total}`}>
-          <label className={styles.label} htmlFor="total">
-            Total:
-          </label>
-          <input
-            className={`${styles.inputField} ${styles.total}`}
-            type="text"
-            id="itemTotal"
-            name="itemTotal"
-            defaultValue={cardData?.itemTotal}
-            onChange={handleChange}
-          />
+        <InputField
+          label="Total"
+          id="itemTotal"
+          name="itemTotal"
+          defaultValue={cardData?.itemTotal}
+          onChange={handleChange}
+          type="text"
+        />
         </div>
       </div>
 
---
+++ src/components/InputField.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './FormComponent.module.css';
+
+function InputField({ label, id, name, defaultValue, onChange, type }) {
+  return (
+    <div className={styles.formField}>
+      <label className={styles.label} htmlFor={id}>
+        {label}:
+      </label>
+      <input
+        className={styles.inputField}
+        type={type}
+        id={id}
+        name={name}
+        defaultValue={defaultValue}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default InputField;
